a663225cb269a9d31daa3156614f70d5aeeefd44
Eder <seikitsi@gmail.com>
2022-05-09 21:22:58 -0400

    feat: feedback parcial.

--- src/js/video.js
+++ src/js/video.js
@@ -140,44 +140,44 @@ const incrementarAvaliacao = (nota, avaliacao) => {
     return avaliacao;
 }
 
-const mostrarMensagem = (texto, tipo) => {
-    // Mostra mensagem tipo popup para o usuário.
-    // Params: texto, é a mensagem a ser mostrada em tela. tipo é o tipo de mensagem.
+// const mostrarMensagem = (texto, tipo) => {
+//     // Mostra mensagem tipo popup para o usuário.
+//     // Params: texto, é a mensagem a ser mostrada em tela. tipo é o tipo de mensagem.
     
-    if (!texto || !tipo) {
-        throw TypeError("Texto ou tipo inválido.");
-    }
+//     if (!texto || !tipo) {
+//         throw TypeError("Texto ou tipo inválido.");
+//     }
 
-    if (tipo !== "sucesso" && tipo !== "erro" && tipo !== "info" && tipo !== "alerta") {
-        throw TypeError("Tipo inválido. Valores aceitos - sucesso, erro, info, alerta");
-    }
+//     if (tipo !== "sucesso" && tipo !== "erro" && tipo !== "info" && tipo !== "alerta") {
+//         throw TypeError("Tipo inválido. Valores aceitos - sucesso, erro, info, alerta");
+//     }
 
-    let mensagemHtml = document.getElementById("mensagem");
-    let classe = "";
-    switch (tipo) {
-        case "sucesso":
-            classe = "alert alert-success";
-            break;
-        case "erro":
-            classe = "alert alert-danger";
-            break;
-        case "info":
-            classe = "alert alert-info";
-            break;
-        case "alerta":
-            classe = "alert alert-warning";
-            break;
-        default:
-            classe = "alert alert-dark";
-    }
+//     let mensagemHtml = document.getElementById("mensagem");
+//     let classe = "";
+//     switch (tipo) {
+//         case "sucesso":
+//             classe = "alert alert-success";
+//             break;
+//         case "erro":
+//             classe = "alert alert-danger";
+//             break;
+//         case "info":
+//             classe = "alert alert-info";
+//             break;
+//         case "alerta":
+//             classe = "alert alert-warning";
+//             break;
+//         default:
+//             classe = "alert alert-dark";
+//     }
 
-    mensagemHtml.innerHTML = texto;
-    mensagemHtml.className = classe;
-    mensagemHtml.hidden = false;
-    setTimeout(() => {
-        mensagemHtml.hidden = true;
-    }, 3000)
-}
+//     mensagemHtml.innerHTML = texto;
+//     mensagemHtml.className = classe;
+//     mensagemHtml.hidden = false;
+//     setTimeout(() => {
+//         mensagemHtml.hidden = true;
+//     }, 3000)
+// }
 
 
 
