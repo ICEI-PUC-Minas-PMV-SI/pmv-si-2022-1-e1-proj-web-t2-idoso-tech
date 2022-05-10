803200a3c4d008dbba885dfad83e39854709afc4
Eder <seikitsi@gmail.com>
2022-05-09 22:26:46 -0400

    Autenticar parcial.

--- src/js/logar.js
+++ src/js/logar.js
@@ -1,60 +1,36 @@
 
-function carregarUsuario(){
-    return fetch("http://localhost:3000/usuarios?email=${email}").then(resposta => {
-        if(resposta.ok && resposta.status === 200) {
+const carregarUsuario = (email) => {
+    return fetch(`http://localhost:3000/usuarios?email=${email}`).then(resposta => {
+        if(resposta.ok && resposta.status == 200) {
             return resposta.json();
         }
-    } ). catch(erro => {
-        console.log(`Erro ao carregar os usuários. ${erro}`);
+
+        if(resposta.status == 404) {
+            alert("Nenhum usuário encontrado.");
+        }
+    }).catch(erro => {
+        alert(`Não foi possível carregar usuário ${erro}`);
     })
 }
 
-function validate() {
+const validate = () => {
     let email = document.getElementById("email").value;
     let senha = document.getElementById("senha").value;
-   
 
+    carregarUsuario(email).then(usuarios => {
+        const usuario = usuarios[0]
+        let logged = false;
 
-    let logged = false;
-
-
-
-    /* GET /posts?title=json-server&author=typicode
-     GET /posts?id=1&id=2
-     GET /comments?author.name=typicode*/
-
-     carregarUsuario().then(usuario => {
-        console.log(usuario);
         if (usuario.email == email && usuario.senha == senha) {
-
-            document.getElementById("form").action = "/src/paginas/video.html";
-            document.getElementById("form").submit();
+            alert("Usuário autenticado");
+            window.location = "/src/paginas/video.html";
+            //document.getElementById("form").action = "/src/paginas/video.html";
+            //document.getElementById("form").submit();
         } else {
-            document.getElementById("form").action = "/src/paginas/logar.html";
-            document.getElementById('msg').innerHTML = `Usuário/senha inválidos `;
+            alert("Senha incorreta");
+            //document.getElementById("form").action = "/src/paginas/logar.html";
+            //document.getElementById('msg').innerHTML = `Usuário/senha inválidos `;
         };
-
-     })
-
-   /* fetch(`http://localhost:3000/usuarios?email=${email}`).then(resposta => {
-        if (resposta.ok && resposta.status == 200) {
-            const usuario = resposta.json();
-            usuario.then(console.log);
-            if (usuario.email == email && usuario.senha == senha) {
-
-                document.getElementById("form").action = "/src/paginas/video.html";
-                document.getElementById("form").submit();
-            } else {
-                document.getElementById("form").action = "/src/paginas/logar.html";
-                document.getElementById('msg').innerHTML = `Usuário/senha inválidos `;
-            };
-
-
-        }
-    }) */
-
-
-  //  const carregarVideos = () => { return fetch("http://localhost:3000/videos").then(resposta => { if (resposta.ok && resposta.status === 200) { return resposta.json(); } if (resposta.status === 404) { mostrarMensagem("mensagem", "Nenhum video encontrado.", "info"); } }).catch(erro => { console.log(`Erro ao carregar os videos. ${erro}`); mostrarMensagem("mensagem", "Não foi possível carregar os feedbacks. Tente mais tarde.", "alerta"); }) }
-
+    })
 
 }
