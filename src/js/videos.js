fd26dc19386ab048784df8b6ff4769ce39bdb900
Eder <seikitsi@gmail.com>
2022-05-11 20:46:08 -0400

    feat: pesquisar vídeos.

--- src/js/videos.js
+++ src/js/videos.js
@@ -1,12 +1,28 @@
 window.onload = () => {
-    loadVideos();
+    getAllVideos();
 }
 
-const getVideos = () => {
+const debounce = (func, timeout = 1000) => {
+    let timer;
+    return (...args) => {
+        clearTimeout(timer);
+        timer = setTimeout(() => {func.apply(this, args);}, timeout);
+    };
+}
+
+const searchVideos = (searchTerm) => {
+    getVideos(`http://localhost:3000/videos?q=${searchTerm}`).then(videos => {
+        loadVideos(videos);
+    })
+}
+
+const search = debounce(searchTerm => {searchVideos(searchTerm);});
+
+const getVideos = (url) => {
     /*
     Retorna os vídeos do db.json
     */
-    return fetch("http://localhost:3000/videos").then(response => {
+    return fetch(url).then(response => {
         if (response.ok) {
             return response.json();
         }
@@ -20,42 +36,46 @@ const getVideos = () => {
     })
 }
 
-const loadVideos = () => {
+const getAllVideos = () => {
+    getVideos("http://localhost:3000/videos").then(videos => {
+        loadVideos(videos);
+    })
+}
+
+const loadVideos = (videos) => {
     /*
     Carrega os vídeos do db.json em tela.
     */
     let cards = "";
-    getVideos().then(videos => {
-        if(videos.length > 0) {
-            videos.forEach(video => {
-                // Cria um card para cada vídeo. Também cria um input hidden para armazenar o id do vídeo.
-                cards += `
-                    <div class="col">
-                        <div class="card shadow-sm">
-                            <input type="hidden" id="video_${video.id}"  value=${video.id}>
-                            <button value="${video.id}" onclick=selectVideo(document.getElementById("video_${video.id}").value)>
-                                <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
-                                xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
-                                preserveAspectRatio="xMidYMid slice" focusable="false">
-                                <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%"
-                                fill="#eceeef" dy=".3em">Thumbnail</text>
-                                </svg>
-                                
-                                <div class="card-body">
-                                <h6>${video.nome}</h6>
-                                <p class="card-text">${video.descricao}</p>
-                                </div>
-                            </button>
-                        </div>
+    if(videos.length > 0) {
+        videos.forEach(video => {
+            // Cria um card para cada vídeo. Também cria um input hidden para armazenar o id do vídeo.
+            cards += `
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <input type="hidden" id="video_${video.id}"  value=${video.id}>
+                        <button value="${video.id}" onclick=selectVideo(document.getElementById("video_${video.id}").value)>
+                            <svg class="bd-placeholder-img card-img-top" width="100%" height="225"
+                            xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail"
+                            preserveAspectRatio="xMidYMid slice" focusable="false">
+                            <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%"
+                            fill="#eceeef" dy=".3em">Thumbnail</text>
+                            </svg>
+                            
+                            <div class="card-body">
+                            <h6>${video.nome}</h6>
+                            <p class="card-text">${video.descricao}</p>
+                            </div>
+                        </button>
                     </div>
-                `;
-            })
-            const cardsCotainerHtml = document.getElementById("cardsContainer");
-            cardsCotainerHtml.innerHTML = cards;
-            return;
-        }
-        mostrarMensagem("mensagem", "Nenhum vídeo encontrado", "alerta");
-    });
+                </div>
+            `;
+        })
+        const cardsCotainerHtml = document.getElementById("cardsContainer");
+        cardsCotainerHtml.innerHTML = cards;
+        return;
+    }
+    mostrarMensagem("mensagem", "Nenhum vídeo encontrado", "alerta");
 }
 
 const selectVideo = (id) => {
