d18fc226f5e8a947f2f5f25333b4858deacbcf49
ginadutra <91692537+ginadev-dutra@users.noreply.github.com>
2022-05-16 15:53:15 -0300

    Alteração de títulos, alteração de avatar e inserção de alert na página logar

---
+++ src/js/dbfake.js
@@ -0,0 +1,27 @@
+var db = {
+    usuarios: [
+        {
+          "id": 1,
+          "nome": "Pedro",
+          "email": "pedro@hotmail.com",
+          "senha": "abc"
+        }, 
+        
+        { 
+           "id": 2,
+           "nome": "Maria",
+           "email": "maria@gmail.com",
+           "senha": "123"
+        }, 
+            
+         { 
+           "id": 3,
+           "nome": "Fatima",
+           "email": "fatima@gmail.com",
+           "senha": "cde"
+        }
+    ]
+
+}
+
+
